bea57fca5fb53d907039370603de06720a846118
Manuel Alejandro Hernandez Marin <manuelalejandrohernandezmarin@gmail.com>
2025-05-04 23:02:17 -0600

    [Feat] - Router added

--- ReadOn/ReadOnApp.swift
+++ ReadOn/ReadOnApp.swift
@@ -9,9 +9,15 @@ import SwiftUI
 
 @main
 struct ReadOnApp: App {
+    
+    let router = Router.shared
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ContentView()
+                    .environmentObject(router)
+            }
         }
     }
 }
