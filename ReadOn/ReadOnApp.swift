1f9d27569a4d8789066bcd381799d366bd22734d
Manuel Alejandro Hernandez Marin <manuelalejandrohernandezmarin@gmail.com>
2025-05-04 22:40:23 -0600

    INIT

---
+++ ReadOn/ReadOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadOnApp.swift
+//  ReadOn
+//
+//  Created by Manuel Alejandro Hernandez Marin on 04/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ReadOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReadOnTests/ReadOnTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReadOnTests.swift
+//  ReadOnTests
+//
+//  Created by Manuel Alejandro Hernandez Marin on 04/05/25.
+//
+
+import Testing
+@testable import ReadOn
+
+struct ReadOnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
